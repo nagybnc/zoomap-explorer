f98e4f6117be4a127108f12007527f174677bce9
nagybnc <bencet630@gmail.com>
2023-03-07 08:15:09 +0100

    Init footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import Footer from "./footer";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
